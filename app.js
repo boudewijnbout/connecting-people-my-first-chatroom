3950763806119de834eb459324bc8024a3e33174
boudewijnbout <boudewijn_bout@outlook.com>
2022-05-23 13:17:59 +0200

    Finished chat v0.1

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 4242
+const http = require("http").createServer(app);
+const path = require("path");
+const io = require("socket.io")(http);
+const port = process.env.PORT || 4242;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Uses
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Create a socket
+io.on("connection", (socket) => {
+  console.log("A random user connected!");
+
+  // User send a message
+  socket.on("message", (message) => {
+    io.emit("message", message);
+  });
+
+  // User disconnects
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+// Render the index view
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Listen on port
+http.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,26 @@
+let socket = io();
+let messages = document.querySelector('section ul');
+let input = document.querySelector('input');
+let form = document.querySelector('form');
+
+// Logics
+form.addEventListener('submit', sendMessage);
+
+/**
+ * Submit create project form with form data to API.
+ *
+ * @function sendMessage
+ * @param {e} event Submit event of the form.
+ */
+function sendMessage(e) {
+    e.preventDefault();
+    
+    if (input.value) {
+        socket.emit('message', input.value);
+        input.value = '';
+    }
+}
+
+socket.on('message', message => {
+    messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }));
+})
